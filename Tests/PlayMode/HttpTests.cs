c730ff347d0cf41988fface662cb599f10b389c2
Harry Hodgson <harry.hodgson@dubitlimited.com>
2019-06-20 11:44:14 +0100

    Removed online connection tests
    This should be mocked, with no real time server connections

--- Tests/PlayMode/HttpTests.cs
+++ Tests/PlayMode/HttpTests.cs
@@ -11,13 +11,11 @@ namespace Duck.Http.Tests.PlayMode
 	public class HttpTests
 	{
 		private Http http;
-		private WaitForEndOfFrame waitForEndOfFrame;
 
 		[OneTimeSetUp]
 		public void Setup()
 		{
 			http = Http.Instance;
-			waitForEndOfFrame = new WaitForEndOfFrame();
 		}
 
 		[OneTimeTearDown]
@@ -44,104 +42,6 @@ namespace Duck.Http.Tests.PlayMode
 			Assert.IsTrue(headers.ContainsKey(HEADER_KEY));
 			Assert.AreEqual(HEADER_VALUE, headers[HEADER_KEY]);
 			Assert.AreEqual(1, headers.Count);
-		}
-
-		[UnityTest]
-		public IEnumerator Expect_OnNetworkError_Event()
-		{
-			// Domain does not exist.
-			const string URI = "http://127.0.0.1:1235";
-
-			var eventTriggered = false;
-
-			Action<HttpResponse> onNetworkError = response => eventTriggered = true;
-
-			var request = Http.Get(URI);
-			request.OnNetworkError(onNetworkError);
-			request.Send();
-
-			yield return new WaitForRequest(request);
-			yield return waitForEndOfFrame;
-
-			Assert.IsTrue(eventTriggered);
-		}
-
-		[UnityTest]
-		public IEnumerator Expect_OnError_Event()
-		{
-			// Existing domain but missing endpoint
-			const string URI = "http://www.google.com/test/http";
-
-			var eventTriggered = false;
-
-			Action<HttpResponse> onError = response => eventTriggered = true;
-
-			var request = Http.Get(URI);
-			request.OnError(onError);
-			request.Send();
-
-			yield return new WaitForRequest(request);
-			yield return waitForEndOfFrame;
-
-			Assert.IsTrue(eventTriggered);
-		}
-
-		[UnityTest]
-		public IEnumerator Expect_OnSuccess_Event()
-		{
-			// Existing domain
-			const string URI = "http://www.google.com";
-
-			var eventTriggered = false;
-
-			Action<HttpResponse> onSuccess = response => eventTriggered = true;
-
-			var request = Http.Get(URI);
-			request.OnSuccess(onSuccess);
-			request.Send();
-
-			yield return new WaitForRequest(request);
-			yield return waitForEndOfFrame;
-
-			Assert.IsTrue(eventTriggered);
-		}
-
-		[UnityTest]
-		public IEnumerator Expect_OnDownloadProgress_Event()
-		{
-			const string URI = "http://www.google.com";
-
-			var eventTriggered = false;
-
-			Action<float> onDownloadProgress = progress => eventTriggered = true;
-
-			var request = Http.GetTexture(URI);
-			request.OnDownloadProgress(onDownloadProgress);
-			request.Send();
-
-			yield return new WaitForRequest(request);
-			yield return waitForEndOfFrame;
-
-			Assert.IsTrue(eventTriggered);
-		}
-		
-		[UnityTest]
-		public IEnumerator Expect_OnUploadProgress_Event()
-		{
-			const string URI = "http://www.google.com";
-
-			var eventTriggered = false;
-
-			Action<float> onUploadProgress = progress => eventTriggered = true;
-
-			var request = Http.Post(URI, string.Empty);
-			request.OnUploadProgress(onUploadProgress);
-			request.Send();
-
-			yield return new WaitForRequest(request);
-			yield return waitForEndOfFrame;
-
-			Assert.IsTrue(eventTriggered);
 		}
 	}
 }
